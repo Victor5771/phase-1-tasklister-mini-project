43524e1e3e2844568649521b22bf67abe635066d
Victor <victor.kyalo@student.moringaschool.com>
2023-09-29 08:33:19 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ // your code here
+ document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskDescription = document.getElementById("new-task-description").value.trim();
+    
+    if (taskDescription) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+      taskList.appendChild(taskItem);
+      document.getElementById("new-task-description").value = "";
+    }
+  });
 });
+
